49ecff71662328f16857d2f645fdcb6717b57dc3
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2016-05-22 19:42:54 -0300

    Add actions for Plays controller

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,42 @@
 class PlaysController < ApplicationController
 
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
   def index
   end
+
+  def show
+  end
   
+  def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(play_params)
+    if @play.save
+      redirect_to play_path(@play)
+    else
+      redner 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
+  end
+
+  def find_play
+    @play = Play.find(params(:id))
+  end
+
 end
